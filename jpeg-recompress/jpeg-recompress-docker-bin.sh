12a852a394b7bbc410431701873f129c34ce7b30
Andreas Schmidt <ash@e.mail.de>
2024-07-19 23:31:49 +0200

    readme

--- jpeg-recompress/jpeg-recompress-docker-bin.sh
+++
@@ -1,3 +0,0 @@
-#!/usr/bin/env bash
-
-docker build -t realash82/zz-jpeg-recompress-bin bin --push
--- jpeg-recompress/jpeg-recompress-docker-extract.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env bash
-
-container_id=$(docker create realash82/zz-jpeg-recompress-bin)
-docker cp "$container_id:/jpeg-recompress" jpeg-recompress
-docker rm "$container_id"
