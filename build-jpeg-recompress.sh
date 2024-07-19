ab6b36a2c578c057b8e186174f5e811493f9fb7b
Andreas Schmidt <ash@e.mail.de>
2024-07-19 22:18:42 +0200

    split containers

--- build-jpeg-recompress.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-docker buildx create --name multiarch --driver docker-container --use
-docker buildx build --platform linux/amd64,linux/arm64 -t realash82/zz-build-jpeg-recompress jpeg-recompress --push
---
+++ jpeg-recompress-build.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+docker build -t realash82/zz-jpeg-recompress-build jpeg-recompress-build --push
